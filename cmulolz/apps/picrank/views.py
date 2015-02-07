5897812a5469e971cfdcf28c5bc6e476d3e10de0
tartanhacks <tartanhacks@bobbert-laptop.local>
2015-02-07 17:24:53 -0500

    update views

--- cmulolz/apps/picrank/views.py
+++ cmulolz/apps/picrank/views.py
@@ -31,10 +31,13 @@ def vote(request):
 
 	if len(picture_set) > 1:
 		picture_pair = random.sample(picture_set, 2)
+		sufficient_picture = True
 	else:
-		picture_pair = picture_set
+		picture_pair = None
+		sufficient_picture = False
 
-	return render(request, 'vote.html', {'pictures': picture_pair})
+	return render(request, 'vote.html', {'pictures': picture_pair;
+		                                 'sufficient': sufficient_picture})
 
 def result(request):
 	date = datetime.datetime.today()
@@ -45,25 +48,23 @@ def result(request):
 	if picture_set > 0:
 		try:
 			winner = Winner.objects.get(date__day = date.day,
-			                              date__month = date.month,
-			                              date__year = date.year)
-			print "123"
+			                            date__month = date.month,
+			                            date__year = date.year)
 			top_votes = winner.picture.vote_set.count()
-			print "middle"
 			top_picture = winner
-			print "321"
 		except:
-			top_votes = 0
+			top_votes = None
 			top_picture = None
 
 		for top in picture_set:
 			if top.vote_set.count() > top_votes:
 				if top_votes > 0:
 					top_picture.picture = top
+					top_votes = top.vote_set.count()
 				else:
-					top_picture = Winner(picture = top, date = datetime.date.today())
+					top_picture = Winner(picture = top, 
+						                 date = datetime.date.today())
 		top_picture.save()
-
 		winner_list = Winner.objects.all().order_by('-date')
 
 	else:
