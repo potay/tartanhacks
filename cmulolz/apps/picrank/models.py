365c9fca8a63a083a50b1034ab134bed0d258a83
Kyungmin Lee <lkm@LKMui-MacBook-Pro.local>
2015-02-07 01:01:38 -0500

    Create basic picture and voting model

--- cmulolz/apps/picrank/models.py
+++ cmulolz/apps/picrank/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Picture(models.Model):
+	#things we need: location, time, date, text
+	timestamp = models.DateTimeField(auto_now_add = True)
+	text = models.TextField(max_length = 500, blank = True)
+	image = models.ImageField()
+
+class Vote(models.Model):
+	timestamp = models.DateTimeField(auto_now_add = True)
+	picture = models.ForeignKey('Picture')
